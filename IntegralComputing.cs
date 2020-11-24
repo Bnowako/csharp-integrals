d399d0d80ee891ee815761bbf84fde1508b3b43e
Burza <burza@iMac-Alina-Pszczokowska.local>
2020-11-24 15:24:57 +0100

    first commit

---
+++ IntegralComputing.cs
@@ -0,0 +1,100 @@
+﻿using System;
+namespace integration
+{
+    public class IntegralComputing
+    {
+
+
+
+        public double CalculateDefiniteIntegral(float[] coefficients)
+        {
+
+            // Approximating definite integral with trapezoidal rule
+            // ab f(x)dx = (b-a) * [ f(a) + f(b) ] / 2
+
+            Console.WriteLine("input range from a to b");
+            bool validAnswer = false;
+            float aNumber = 0;
+            float bNumber = 0;
+
+            while (!validAnswer)
+            {
+                Console.Write("Input a: ");
+                string a = Console.ReadLine();
+                Console.Write("Input b: ");
+                string b = Console.ReadLine();
+                bool validA = float.TryParse(a, out aNumber);
+                bool validB = float.TryParse(b, out bNumber);
+
+                if (validA && validB && bNumber > aNumber)
+                {
+                    validAnswer = true;
+                }
+            }
+            float bMinA = bNumber - aNumber;
+
+            double funcA = CalculateFunction(coefficients, aNumber);
+            Console.WriteLine("\n\n FUNC A = {0}", funcA);
+            double funcB = CalculateFunction(coefficients, bNumber);
+            Console.WriteLine("\n\n FUNC B = {0}", funcB);
+
+            double funcAPlusFuncB = funcA + funcB;
+            double result = bMinA * (funcAPlusFuncB / 2);
+
+            return result;
+
+        }
+        public double CalculateFunction(float[] coefficients, float x)
+        {
+            double result = 0;
+
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                if (coefficients[i] != 0)
+                {
+                    double temp = 0;
+                    temp = Math.Pow(x, i + 1) * coefficients[i];
+                    result += temp;
+                }
+            }
+
+            return result;
+        }
+
+        public string CalculateIndefiniteIntegral(float[] coefficients)
+        {
+            string result = "Integration result = ";
+            string[] integralReslut = new string[coefficients.Length];
+            for (int i = 0; i < coefficients.Length; i++)
+            {
+                if (coefficients[i] != 0)
+                {
+                    string integration = "";
+                    int nPlusOne = i + 2;
+                    integration += "(" + coefficients[i].ToString() + "/"
+                        + nPlusOne.ToString() + ")" + "x^" + nPlusOne.ToString();
+
+                    result += integration;
+                    if (i != coefficients.Length - 1)
+                    {
+                        result += " + ";
+                    }
+                    else if (i == coefficients.Length - 1)
+                    {
+                        result += " + C ";
+                    }
+
+                }
+            }
+
+            return result;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+
+namespace integration
+{
+
+    public static class Globals
+    {
+        public static bool RunProgram = true;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntegralComputing integralComputing = new IntegralComputing();
+
+
+            while (Globals.RunProgram)
+            {
+
+                int highestPower = GetPolynomialGrade();
+                float[] coefficients;
+                string function;
+                (coefficients, function) = GetCoefficients(highestPower);
+
+                Console.WriteLine(function);
+                bool validChoice = false;
+                string resultIndefinite = "";
+                double resultDefinite = 0;
+
+                while (!validChoice)
+                {
+                    Console.WriteLine("Input");
+                    Console.WriteLine("1 if you want to calculate definite integral");
+                    Console.WriteLine("2 if you want to calculate indefine integral");
+                    Console.Write("Chosen integral: ");
+                    string choice = Console.ReadLine();
+                    if (choice == "1")
+                    {
+                        validChoice = true;
+                        resultDefinite = integralComputing.CalculateDefiniteIntegral(coefficients);
+                        Console.WriteLine("\n\n----- RESULT ------");
+                        Console.WriteLine(resultDefinite);
+                        Console.WriteLine("-------------------\n\n");
+                    }
+                    else if(choice == "2")
+                    {
+                        validChoice = true;
+                        resultIndefinite = integralComputing.CalculateIndefiniteIntegral(coefficients);
+                        Console.WriteLine(resultIndefinite);
+                    }
+                }
+
+            }
+
+        }
+
+
+        static int GetPolynomialGrade()
+        {
+
+            Console.WriteLine("Input highest power of your polynomial func (max 10)");
+
+            bool isNumber = false;
+            int highestPower = 0;
+
+            while (!isNumber)
+            {
+                Console.Write("Input number 1 - 10 : ");
+
+                string userInput = Console.ReadLine();
+
+                isNumber = Int32.TryParse(userInput, out highestPower);
+
+                if (highestPower > 10 || highestPower < 1)
+                {
+                    isNumber = false;
+                }
+
+            }
+
+            return highestPower;
+        }
+
+        static (float [],string) GetCoefficients(int highestPower)
+        {
+            string wholeFunction = "f(x) = ";
+            float [] coefficients = new float[highestPower];
+            for (int i = 1; i <= highestPower; i++)
+            {
+                bool validAnswer = false;
+                float coefficientNumber = 0;
+                string coefficient = "";
+                while (!validAnswer)
+                {
+                    Console.Write("Input coefficient before x^{0} ", i);
+                    coefficient = Console.ReadLine();
+                    validAnswer = float.TryParse(coefficient, out coefficientNumber);
+                }
+                string currentX = coefficient + "x^" + i.ToString();
+                if (i != highestPower)
+                {
+                    currentX = currentX + " + ";
+                }
+                wholeFunction = String.Concat(wholeFunction, currentX);
+
+                coefficients[i - 1] = coefficientNumber;
+                
+            }
+            return (coefficients, wholeFunction);
+        }
+    }
+}
